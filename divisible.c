78cb656ff678974903645aafee93ac327ecaa482
harishkumar066 <58950050+harishkumar066@users.noreply.github.com>
2019-12-19 08:58:42 +0530

    Add files via upload

---
+++ divisible.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%5==0&&n%11==0)
+    printf("it is divisible by 5& 11");
+    else
+    printf("not divisible by 5 &11");
+    return 0;
+    
+}
